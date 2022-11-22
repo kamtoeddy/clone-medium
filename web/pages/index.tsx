da4988e9849cdf28c9b0f6486e0b8114262b25b1
kamtoeddy <lollipopkalog@gmail.com>
2022-11-22 18:44:58 +0100

    updated typo on home page

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -27,7 +27,7 @@ const Home = ({ posts }: Props) => {
             is a place to write, read and connect
           </h1>
           <h2>
-            It's easy and free to post your thinking on any topic an connect
+            It's easy and free to post your thinking on any topic and connect
             with millions of readers.
           </h2>
         </div>
